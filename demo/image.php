9437eb279909f2f55aa40ffc86ddd648a880823d
eloy alejandro <eloyalejandro@gmail.com>
2016-03-26 12:59:06 -0430

    Actualizacion de contenidos Internos
    s

--- demo/image.php
+++ demo/image.php
@@ -1,12 +1,14 @@
+
 <?php
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Cache-Control: no-cache");
 header("Pragma: no-cache");
-$imagepath="http://www.visiongraphstudios.com/bartender/demo.jpg";
+$imagepath="http://www.visiongraphstudios.com/exiauto/demo.jpg?random=323527528432525.24234";
 
 $image=imagecreatefromjpeg($imagepath);
 
 header('Content-Type: image/jpeg');
 imagejpeg($image, NULL, 95);
 ?>
+
 
