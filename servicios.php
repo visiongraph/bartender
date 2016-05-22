56ce20efa8ce16f23bd323d48834c57d2b0da27b
Robert Ochoa <robert.ochoa@gmail.com>
2016-05-22 17:37:38 -0430

    Forma de solicitud de presupuesto
    formulario

--- servicios.php
+++ servicios.php
@@ -1,6 +1,6 @@
 <?php
 	function getTemplate(){
-		$ruta = 'mail/contacto_template.php';
+		$ruta = 'mail/solicitud_template.php';
 		if (is_file($ruta)) {
 			ob_start();
 			include $ruta;
@@ -16,15 +16,26 @@
 		$mail->Host = '127.0.0.1';
 		$mail->Host = 'localhost';
 		$mail->SetFrom('contacto@bartender.com', 'Contacto desde la pagina web');				
-		$mail->AddAddress('eloyalejandro@gmail.com');
+		//$mail->AddAddress('eloyalejandro@gmail.com');
+		$mail->AddAddress('robert.ochoa@gmail.com');
 		$mail->IsHTML(true);
 		$mail->CharSet = 'UTF-8';
 		$body = getTemplate();		
 	
-		$mail->Subject = 'Forma de Contacto';
+		$mail->Subject = 'Solicitud de presupuesto';
 				
-		$body = sprintf($body,$_POST['nombre_bar'],$_POST['email_bar'],$_POST['telefono_bar'],$_POST['mensaje_bar']);
-		$mail->Body = $body;
+		$body = sprintf($body,
+						$_POST['nombre'],
+						$_POST['email'],
+						$_POST['telefono'],
+						$_POST['tipo_evento'],
+						$_POST['empresa'],
+						$_POST['cant_invitados'],
+						$_POST['fecha_evento'],
+						$_POST['dir_evento'],
+						$_POST['tipo_servicio'],
+						$_POST['detalles']);
+		$mail->Body = $body; 
 		
 		if(!$mail->Send()) {
 			var_dump($mail->ErrorInfo);
@@ -33,7 +44,6 @@
 		}	
 	}
 ?>
-
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
@@ -70,37 +80,60 @@
 		<style type="text/css" title="dynamic-css" class="options-output">
 			body{text-align:inherit;line-height:undefinedpx;font-weight:400;}
 			h1,h2,h3,h4,h5{line-height:undefinedpx;}
-		</style>
-		
+		</style>            
 		<script>
 			function validateForm() {
-				var x = document.forms["forma_contacto"]["nombre_bar"].value;
+				var x = document.forms["forma_solicitud"]["nombre"].value;
 				if (x == null || x == "") {
-					alert("Debe colocar el Nombre");
+					alert("Debe colocar el Nombre Completo");
 					return false;
 				}
-				var x = document.forms["forma_contacto"]["email_bar"].value;
+				var x = document.forms["forma_solicitud"]["telefono"].value;
+				if (x == null || x == "") {
+					alert("Debe colocar el teléfono");
+					return false;
+				}
+				var x = document.forms["forma_solicitud"]["email"].value;
 				if (x == null || x == "") {
 					alert("Debe colocar el Email");
 					return false;
 				}
-				var x = document.forms["forma_contacto"]["mensaje_bar"].value;
+				
+				var x = document.forms["forma_solicitud"]["tipo_evento"].value;
 				if (x == null || x == "") {
-					alert("Debe colocar el Mensaje");
+					alert("Debe indicar el tipo de evento");
+					return false;
+				}
+				var x = document.forms["forma_solicitud"]["cant_invitados"].value;
+				if (x == null || x == "") {
+					alert("Debe indicar la cantidad de invitados");
+					return false;
+				}
+				var x = document.forms["forma_solicitud"]["fecha_evento"].value;
+				if (x == null || x == "") {
+					alert("Debe indicar la fecha del evento");
+					return false;
+				}
+				var x = document.forms["forma_solicitud"]["dir_evento"].value;
+				if (x == null || x == "") {
+					alert("Debe indicar la direcció del evento");
+					return false;
+				}	
+				var x = document.forms["forma_solicitud"]["tipo_servicio"].value;
+				if (x == null || x == "") {
+					alert("Debe indicar el tipo de servicio deseado");
 					return false;
 				}				
 			}		
-		</script>
-		
+		</script>		
 	</head>
 	<body class="home page page-id-1839 page-template page-template-template-front-page-php"> 
 		<?php if ($enviado) { ?>		
 			<div id="caja-mensaje">
-				 Su mensaje se envió con éxito. Responderemos a su inquietud a la brevedad &nbsp;&nbsp;&nbsp;&nbsp;
+				 Su solicitud se envió con éxito. Un representante de ventas se pondr&aacute; en contacto con usted a la brevedad &nbsp;&nbsp;&nbsp;&nbsp;
 				 <input type="button" value="OK" class="wpcf7-form-control wpcf7-submit" onclick="document.getElementById('caja-mensaje').hidden = true;"/>
 			</div>
-		<?php } ?>
-	
+		<?php } ?>	
 		<div id="main-wrapper">
 			<header id="header">
 				<div class="header-top-bar">
@@ -166,8 +199,8 @@
 									<li id="menu-item-2610" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-2610 dropdown">
 										<a title="MULTIMEDIA" href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" style="transition: none;">MULTIMEDIA <span class="caret"></span></a>
 										<ul role="menu" class=" dropdown-menu">
-											<li id="menu-item-2582" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2582"><a title="Submenu" href="videos.html">Vídeos</a></li>
-											<li id="menu-item-2588" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2588"><a title="Submenu" href="galeria.html">Galería</a></li>
+											<li id="menu-item-2582" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2582"><a title="Vídeos" href="videos.html">Vídeos</a></li>
+											<li id="menu-item-2588" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2588"><a title="Galerías" href="galerias.html">Galería</a></li>
 										</ul>
 									</li>
 									<li id="menu-item-2418" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2418">
@@ -190,105 +223,159 @@
 									<a rel="v:url" property="v:title" href="index.html">Inicio</a>
 								</span>&nbsp;&nbsp; 
 								<i class="fa fa-chevron-right"></i> &nbsp;&nbsp;
-								<span class="current">Contacto</span>
+								<span class="current">Servicios</span>
 							</li>
 						</ul>
 					</div>
 				</div>
 			</header>
 			
-			<div class="page-content">
-				<input type="hidden" id="map_long" name="map_long" value="-66.81803">
-				<input type="hidden" id="map_lat" name="map_lat" value="10.48052">
-				<div class="map-section">
-					<div id="map_canvas_go"></div>
+			<div class="page-content">        
+				<div class="headingser">                
+					<h1>Servicios</h1>            
 				</div>
-			<div class="contact-us">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-6">
-							<div class="contact-details">
-								<h4>Contácto</h4>
-								<div class="row">
-									<div class="col-md-12 col-sm-8">
-										<h5>Datos de contácto</h5>
-										<div class="address clearfix">
-											<i class="fa fa-map-marker"></i>
-											<p> Torre Profesional La California, PH 1 Caracas Venezuela</p>
-										</div>
-										<div class="address clearfix">
-											<i class="fa fa-phone"></i>
-											<p>(0212)271.01.86</p>
-										</div>										
-										<div class="time-to-open clearfix">
-											<i class="fa fa-clock-o"></i>
-											<p>Lunes a Viernes: 8am - 4pm <br>
-Sábados: 8am - 4pm <br>
- Domingos: 10am - 5pm</p>
+				<div class="news-events-blog">    
+					<div class="container">                    
+						<div class="row">                        
+							<div class="col-md-8">
+								<a name="serv1"></a>
+								<div id= "post-2513" class="post-2513 news type-news status-publish has-post-thumbnail hentry blog-list">                                    
+									<div class="row">                                        
+										<div class="col-md-4 col-sm-4">                                    
+											<div class="blog-list-img">
+												<img src= "imagenes/home/img-bartenderbar2.jpg" alt= "news-event-image">                                                                                                        
+											</div>                                            
+										</div>                                    
+										<div class="col-md-8 col-sm-8">
+											<h5><a href="#">Bartender Profesional</a></h5>                                                
+											<!--<ul class="list-inline">
+												<li class="place">
+													<i class="fa fa-map-marker"></i><span class="bl-sort">in</span>Cincinnati, Ohio                                                    
+												</li>                                                                                                
+												<li class="date">
+													<i class="fa fa-calendar"></i>November 23, 2014 at 8:47 pm                                                    
+												</li>
+											</ul>-->
+											<div class="bl-sort">
+												<p>
+													Sweet turkish, ristretto rich, fair trade, trifecta, latte eu variety single shot 
+													robusta ristretto. Filter sit, caffeine foam half and turkish, ristretto rich, fair 
+													trade, trifecta, latte eu variety single shot robusta ristretto. Filter sit, caffeine 
+													foam half and<br>
+												
+												</p>                                                     
+											</div>                                                                                         
 										</div>
 									</div>
-									<!--
-									<div class="col-md-6 col-sm-6">
-										<h5>some other place</h5>
-										<div class="address clearfix">
-											<i class="fa fa-map-marker"></i>
-											<p>Viale della Moschea,85 00197 Roma Italy</p>
-										</div>
-										<div class="time-to-open clearfix">
-											<i class="fa fa-clock-o"></i>
-											<p>Monday - Friday:9am - 11pm Saturday:10am - till last customer Sunday:10am - till last customer</p>
-										</div>
-									</div>-->
 								</div>
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="wpcf7" id="wpcf7-f46-o1" lang="en-US" dir="ltr">
-								<div class="screen-reader-response"></div>
-									<form name="forma_contacto" action="contacto.php" method="post" class="wpcf7-form"  onsubmit="return validateForm();">
-										<div class="send-message">
-											<h4>Envíanos un mensaje</h4>
-											<div class="row">
-												<div class="col-md-12">
-													<p>
-														<span class="wpcf7-form-control-wrap your-name">
-															<input type="text" name="nombre_bar" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" placeholder="Nombre*" />
-														</span>
-													</p>
-												</div>
-												<div class="col-md-6 col-sm-6">
-													<p>
-														<span class="wpcf7-form-control-wrap your-email">
-															<input type="email" name="email_bar" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" placeholder="Email*" />
-														</span>
-													</p>
-												</div>
-												<div class="col-md-6 col-sm-6">
-													<p>
-														<span class="wpcf7-form-control-wrap your-subject">
-															<input type="text" name="telefono_bar" value="" size="40" class="wpcf7-form-control wpcf7-text" placeholder="Teléfono" />
-														</span>
-													</p>
-												</div>
+								<a name="serv2"></a>
+								<div id= "post-2227" class="post-2227 news type-news status-publish has-post-thumbnail hentry blog-list">
+									<div class="row">
+										<div class="col-md-4 col-sm-4">
+											<div class="blog-list-img">
+												<img src= "imagenes/home/img-luxurybar2.jpg" alt= "news-event-image">
 											</div>
-											<p><!-- end nasted .row --></p>
-											<p>
-												<span class="wpcf7-form-control-wrap your-message">
-													<textarea name="mensaje_bar" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" placeholder="Tu mensaje"></textarea>
-												</span>
-											</p>
-											<p class="button-submit"><input type="submit" value="Enviar" class="wpcf7-form-control wpcf7-submit" /></p>
 										</div>
-										<div class="wpcf7-response-output wpcf7-display-none"></div>
-									</form>
+										<div class="col-md-8 col-sm-8">
+											<h5><a href="#">Luxury Bar</a></h5>
+											<!--<ul class="list-inline">
+												<li class="place"><i class="fa fa-map-marker"></i><span class="bl-sort">in</span>Baltimore, Maryland</li>
+												<li class="date"><i class="fa fa-calendar"></i>November 16, 2014 at 9:07 am</li>
+											</ul>-->
+											<div class="bl-sort">
+												<p>
+													<b>¿Estas buscando exclusividad, elegancia  e innovación en un solo servicio?</b> <br><br>
+													Para ello te ofrecemos nuestro servicio Luxury bar donde podrás disfrutas del Glamour y 
+													el lujo de los más exquisitos cocteles,  y las más modernas tendencias de Coctelería a 
+													nivel mundial. Con este servicio también le brindamos la opción de tener una barra de 
+													vinos en su evento utilizando los mejores productos del mercado.<br>													
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<a name="serv3"></a>
+								<div id= "post-2091" class="post-2091 news type-news status-publish has-post-thumbnail hentry tag-tag1 tag-tag2 blog-list">
+									<div class="row">
+										<div class="col-md-4 col-sm-4">
+											<div class="blog-list-img">
+												<img src= "imagenes/home/img-platinumbar2.jpg" alt= "news-event-image">
+											</div>
+										</div>
+										<div class="col-md-8 col-sm-8">
+											<h5><a href="#">Platinum Bar</a></h5>
+											<!--<ul class="list-inline">
+												<li class="place"><i class="fa fa-map-marker"></i><span class="bl-sort">in</span>Baltimore, Maryland</li>
+												<li class="date"><i class="fa fa-calendar"></i>November 16, 2014 at 9:07 am</li>
+											</ul>-->
+											<div class="bl-sort">
+												<p>
+													Diseñado para ofrecer experiencias inolvidables, donde la elegancia es nuestra bandera para hacer de su evento una noche inolvidable. Nuestros cocteles platinium están preparados con productos e ingredientes Premium, buscando siempre complacer los paladares más exigentes. <br>
+													
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+														
+							</div>
+							<div class="col-md-4">
+                            
+								<div class="events-side-panel">
+									<div class="widget">
+										<h5>Solicitar Presupuesto</h5> <br>    
+										<p style="text-align:justify; font-size:14px; color:#5e5e5e">
+											Por favor rellenar formulario a continuación<b></b>
+										</p>   
+										<div class="blog-latest">
+											<div class="row">
+												<form name="forma_solicitud" action="servicios.php" method="post" class="searchform"  onsubmit="return validateForm();">
+													<div class="search-keyword">
+														<span class="obligatorio">*</span> Nombre y Apellido:<br>
+														<input type="text" placeholder="Nombre Completo" value="" maxlength="50" name="nombre" id="nombre" /><br>
+														<span class="obligatorio">*</span> Teléfono:
+														<input type="text" placeholder="Codigo de Area y Teléfono" value="" maxlength="20" name="telefono" id="telefono" /><br>
+														<span class="obligatorio">*</span> Correo:
+														<input type="text" placeholder="Correo válido" value="" name="email" maxlength="50" id="email" /><br>
+														<span class="obligatorio">*</span> Tipo de Evento:<br>
+														<select name="tipo_evento" id="tipo_evento">
+															<option value="Corporativo">Evento Corporativo</option>
+															<option value="familiar">Evento Familiar</option>
+														</select><br>
+														Nombre de la empresa:
+														<input type="text" placeholder="Si es un evento corporativo" value="" maxlength="70" name="empresa" id="empresa" /><br>
+														<span class="obligatorio">*</span> Cantidad de Invitados:
+														<input type="text" placeholder="Número de invitados al evento" maxlength="6"  value="" name="cant_invitados" id="cant_invitados" />
+														<span class="obligatorio">*</span> Fecha del Evento:<br>
+														<input type="text" placeholder="dd/mm/aaaa ejemplo: 08/11/1978" value="" maxlength="10" name="fecha_evento" id="fecha_evento" /><br>
+														<span class="obligatorio">*</span> Dirección del Evento:<br>
+														<input type="text" placeholder="ejemplo: Urb. Moltalban1 / Caracas" value="" maxlength="100" name="dir_evento" id="dir_evento" />
+														<span class="obligatorio">*</span> Selecciona Servicio Deseado:<br>
+														<select id="tipo_servicio" name="tipo_servicio">
+															<option value="Bartender_Profesional">Barra Bartender Profesional</option>
+															<option value="Luxury">Luxury Bar</option>
+															<option value="Kids">Kids Bar</option>
+														</select><br>
+														Detalles del Evento Mensaje:<br>
+														<textarea name="detalles" cols="" rows="8" id="detalles" maxlength="200"></textarea>
+														<p class="button-submit" style="width:auto !important;">
+															<input type="submit" value="Enviar" class="wpcf7-form-control wpcf7-submit aligncenter" />														
+														</p>
+														<div style="text-align: right; font-size: 10px; width:100%; margin-bottom: -25px;">(<span class="obligatorio">*</span>) Campo obligatorio</div>
+													</div>
+												</form>
+												<div class="nota"></div>
+											</div>
+										</div>									
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			
-			<!--footer start-->
+    
+			<!--footer start-->  
 			<footer id="footer">
 				<div class="container">
 					<div class="main-footer">
@@ -364,7 +451,6 @@ Tubartender Bar Academy, La California Salida de la estación del metro la Calif
 		</div>
 		<script type='text/javascript' src='js/comment-reply.min.js?ver=4.0.8'></script>
 		<script type='text/javascript' src='js/jquery.form.min.js?ver=3.51.0-2014.06.20'></script>
-		
 		<script type='text/javascript' src='js/add-to-cart.min.js?ver=2.2.8'></script>
 		<script type='text/javascript' src='js/jquery.blockUI.min.js?ver=2.60'></script>
 		<script type='text/javascript' src='js/woocommerce.min.js?ver=2.2.8'></script>
